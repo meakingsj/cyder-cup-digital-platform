4e8e2648f79c469094051c8e45780d070e92eee6
JJ Meakings <meakingsj@gmail.com>
2026-08-01 18:54:36 -0700

    Fix analytics integration

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,18 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/react";
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
 
+import App from "./App";
 import "./styles/globals.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-<StrictMode>
-  <App />
-  <Analytics />
-  <SpeedInsights />
-</StrictMode>
+createRoot(
+  document.getElementById("root")!,
+).render(
+  <StrictMode>
+    <App />
+    <Analytics />
+    <SpeedInsights />
+  </StrictMode>,
 );
